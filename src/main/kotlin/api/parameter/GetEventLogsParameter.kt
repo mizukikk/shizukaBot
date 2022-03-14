4c43a05ec63f864e13bd21774fc77a7cad801157
mizukikk <kk801102>
2022-03-14 15:19:15 +0800

    修改 events api 自動帶入預設 ranks

--- src/main/kotlin/api/parameter/GetEventLogsParameter.kt
+++ src/main/kotlin/api/parameter/GetEventLogsParameter.kt
@@ -5,6 +5,6 @@ import api.enum.EventType
 data class GetEventLogsParameter(
     val id: Int,
     val type: EventType,
-    val ranks: String,
+    val ranks: String = "100,2500,5000,10000,25000,50000",
     val timeMin: String? = null
 )
